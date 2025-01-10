88ebec900823c52b639433cc3202e182b2cfd2a3
jialin <mr.liujalo@gmail.com>
2025-01-10 14:12:58 +0800

    安装使用axios

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,73 @@
-<script setup>
+<script setup name="login">
+import login_image from "@/assets/image/login.png";
+import axios from "axios";
+import { reactive } from "vue";
 
+let form = reactive({
+  email: "",
+  password: "",
+});
+
+const onSubmit = () => {
+  axios
+    .post("http://localhost:8000/auth/login/", form)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      let detail = err.response.data.detail;
+      alert(detail);
+    });
+};
 </script>
 
 <template>
-login
+  <div class="dowebok">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        <div class="login100-pic js-tilt" data-tilt>
+          <img :src="login_image" alt="IMG" />
+        </div>
+
+        <div class="login100-form validate-form">
+          <span class="login100-form-title"> 员工登陆 </span>
+
+          <div class="wrap-input100 validate-input">
+            <input
+              class="input100"
+              type="text"
+              name="email"
+              placeholder="邮箱"
+              v-model="form.email"
+            />
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="iconfont icon-fa-envelope" aria-hidden="true"></i>
+            </span>
+          </div>
+
+          <div class="wrap-input100 validate-input">
+            <input
+              class="input100"
+              type="password"
+              name="password"
+              placeholder="密码"
+              v-model="form.password"
+            />
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="iconfont icon-fa-lock" aria-hidden="true"></i>
+            </span>
+          </div>
+
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn" @click="onSubmit">登陆</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped src="@/assets/css/login.css"></style>
+<style scoped src="@/assets/iconfont/iconfont.css"></style>
